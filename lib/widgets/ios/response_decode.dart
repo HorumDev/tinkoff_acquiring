f06976721c6af5a618383727529e107913287ea9
Roman <225000@niuitmo.ru>
2021-12-13 17:19:39 +0300

    ios fix

--- lib/widgets/ios/response_decode.dart
+++ lib/widgets/ios/response_decode.dart
@@ -1,10 +1,18 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:tinkoff_acquiring/widgets/android/info_toast.dart';
 import 'package:tinkoff_acquiring/wrappers/data/coding_keys.dart';
 import 'package:tinkoff_acquiring/wrappers/data/payment_status.dart';
 
-PaymentStatus? iosResponseDecode(String result){
+PaymentStatus? iosResponseDecode(result){
+
+  if(result is Array) {
+    final decoded = jsonDecode(result.toString());
+    final success = decoded[code_success];
+    final status = decoded[code_status];
+    return decodingMap[status];
+  }
   //костыль
   //TODO return error from swift as JSON
   if(result.contains(code_success) || result.contains(code_success.toLowerCase())) {
